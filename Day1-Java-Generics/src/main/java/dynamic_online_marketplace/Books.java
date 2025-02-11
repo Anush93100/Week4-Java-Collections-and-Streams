a11c2178272c936021ab6d98ce58fc19118713f5
Anush Tenguriya <anushtenguriya93100@gmail.com>
2025-02-11 12:46:36 +0530

    [Anush] ADD. completed creating dynamic online marketplace system by using generics

---
+++ Day1-Java-Generics/src/main/java/dynamic_online_marketplace/Books.java
@@ -0,0 +1,7 @@
+package dynamic_online_marketplace;
+
+public class Books extends Category{
+    Books(String name, double price){
+        super("Book",name,price);
+    }
+}
---
+++ Day1-Java-Generics/src/main/java/dynamic_online_marketplace/Gadgets.java
@@ -0,0 +1,7 @@
+package dynamic_online_marketplace;
+
+public class Gadgets extends Category{
+    Gadgets(String name, double price){
+        super("Gadget",name,price);
+    }
+}
