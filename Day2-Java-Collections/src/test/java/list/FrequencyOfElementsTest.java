03292c7e1bd07180a1e94f5d486ea9da4861f134
Anush Tenguriya <anushtenguriya93100@gmail.com>
2025-02-13 11:40:11 +0530

    [Anush] ADD. completed counting frequency of elements problem

---
+++ Day2-Java-Collections/src/test/java/list/FrequencyOfElementsTest.java
@@ -0,0 +1,28 @@
+package list;
+
+import list.frequency_of_elements.FrequencyOfElements;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FrequencyOfElementsTest {
+
+    @Test
+    void findingFrequencyTest(){
+        List<String> list=new ArrayList<>();
+        list.add("apple");
+        list.add("banana");
+        list.add("apple");
+        list.add("orange");
+        Map<String ,Integer> map= FrequencyOfElements.countFrequency(list);
+        Map<String, Integer> expected = new HashMap<>();
+        expected.put("apple", 2);
+        expected.put("banana", 1);
+        expected.put("orange", 1);
+        assertEquals(map,expected);
+    }
+}
