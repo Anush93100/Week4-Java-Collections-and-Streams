51866703f5f5129ea0a7573893ee9a09322f640c
Anush Tenguriya <anushtenguriya93100@gmail.com>
2025-02-13 11:45:16 +0530

    [Anush] ADD. completed reversing a list problem

---
+++ Day2-Java-Collections/src/test/java/list/ReverseAListTest.java
@@ -0,0 +1,31 @@
+package list;
+
+import list.reverse_a_list.ReverseAList;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ReverseAListTest {
+
+    @Test
+    void reversingArrayListTest(){
+        List<Integer> arrayList=new ArrayList<>(Arrays.asList(1,2,3,4,5));
+        ReverseAList.reversingList(arrayList);
+
+        List<Integer> expected=Arrays.asList(5,4,3,2,1);
+        assertEquals(arrayList,expected);
+    }
+
+    @Test
+    void reversingLinkListTest(){
+        List<Integer> arrayList=new LinkedList<>(Arrays.asList(1,2,3,4,5));
+        ReverseAList.reversingList(arrayList);
+
+        List<Integer> expected=Arrays.asList(5,4,3,2,1);
+        assertEquals(arrayList,expected);
+    }
+}
