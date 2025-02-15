6a38fcbc23750981f113b3a34e804a74a14ae779
Anush Tenguriya <anushtenguriya93100@gmail.com>
2025-02-15 21:13:09 +0530

    [Anush] ADD. program to extract programming language name from a text

---
+++ Day5-Java-Regex-and-JUnit/src/test/java/regex/advanced_problems/extract_programming_language_names/ExtractProgrammingLanguageNamesTest.java
@@ -0,0 +1,31 @@
+package regex.advanced_problems.extract_programming_language_names;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+public class ExtractProgrammingLanguageNamesTest {
+
+    @Test
+    void testCase1(){
+        String text="I love Java, Python, and JavaScript, but I haven't tried Go yet.";
+        Set<String> programmingLang=ExtractProgrammingLanguageNames.extractingProgrammingLanguage(text);
+
+        List<String> expected= Arrays.asList("Java","Python","JavaScript","Go");
+        assertArrayEquals(programmingLang.toArray(),expected.toArray());
+    }
+
+
+    @Test
+    void testCase2(){
+        String text="but I haven't tried yet.";
+        Set<String> programmingLang=ExtractProgrammingLanguageNames.extractingProgrammingLanguage(text);
+
+        List<String> expected= new ArrayList<>();
+        assertArrayEquals(programmingLang.toArray(),expected.toArray());
+    }
+}
