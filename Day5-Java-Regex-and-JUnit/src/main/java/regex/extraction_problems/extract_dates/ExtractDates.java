170cd066da8c7276b16cae160fbf15d0df065e3c
Anush Tenguriya <anushtenguriya93100@gmail.com>
2025-02-15 21:25:54 +0530

    [Anush] ADD. program to extract dates in format dd/mm/yyyy from text

---
+++ Day5-Java-Regex-and-JUnit/src/main/java/regex/extraction_problems/extract_dates/ExtractDates.java
@@ -0,0 +1,41 @@
+package regex.extraction_problems.extract_dates;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExtractDates {
+    public static List<String> extractingDate(String sentence){
+       String regex= "\\b(0[1-9]|[1-2][0-9]|3[0-1])/(0[1-9]|1[0-2])/\\d{4}\\b";
+        Pattern pattern=Pattern.compile(regex);
+        Matcher matcher=pattern.matcher(sentence);
+
+        List<String> dates=new ArrayList<>();
+        while(matcher.find()){
+            dates.add(matcher.group());
+        }
+        return dates;
+    }
+
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+
+        System.out.println("Enter the sentence: ");
+        String sentence=input.nextLine();
+
+        List<String> dates=extractingDate(sentence);
+
+        if(dates.isEmpty()){
+            System.out.println("No dates in format dd/mm/yyyy found.");
+        }else{
+            System.out.println("Dates in format dd/mm/yyyy in given sentence are:");
+            for (String date:dates){
+                System.out.println(date);
+            }
+        }
+
+        input.close();
+    }
+}
