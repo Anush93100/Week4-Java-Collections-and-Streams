80bbeb5452615696ef00ca76e300b0ec36c643cf
Anush Tenguriya <anushtenguriya93100@gmail.com>
2025-02-15 20:54:42 +0530

    {Anush] ADD. program to demonstrate Exception propagation in methods

---
+++ Day-4-Java-Exceptions/src/test/java/exception_propagation_in_methods/ExceptionPropagationInMethodsTest.java
@@ -0,0 +1,24 @@
+package exception_propagation_in_methods;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class ExceptionPropagationInMethodsTest {
+
+    @Test
+    void propagationTest(){
+        Exception exception=assertThrows(ArithmeticException.class,()->ExceptionPropagationInMethods.method2(10,0));
+        assertEquals("/ by zero",exception.getMessage());
+    }
+
+    @Test
+    void propagationSuccessfulTest(){
+        ByteArrayOutputStream output=new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+        ExceptionPropagationInMethods.method2(10,2);
+        assertEquals("Division:5",output.toString().trim());
+    }
+}
