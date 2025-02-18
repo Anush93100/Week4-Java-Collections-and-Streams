775490b3ac7d68f9b4fa5971d5c798cf04a14486
Anush Tenguriya <anushtenguriya93100@gmail.com>
2025-02-18 16:40:05 +0530

    [Anush] ADD. program for dynamic method invocation

--- Day6-Java-Annotations-and-Reflection/src/main/java/reflection/intermediate_level/access_and_modify_static_fields/Configuration.java
+++ Day6-Java-Annotations-and-Reflection/src/main/java/reflection/intermediate_level/access_and_modify_static_fields/Configuration.java
@@ -1,7 +1,7 @@
 package reflection.intermediate_level.access_and_modify_static_fields;
 
 public class Configuration {
-    private String API_KEY;
+    private static String API_KEY;
 
     public Configuration(String API_KEY) {
         this.API_KEY=API_KEY;
